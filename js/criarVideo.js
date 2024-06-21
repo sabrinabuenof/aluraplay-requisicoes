fc8e2bdff50238cf7e3695509a8281d06554aac6
Sabrina Bueno <bina_fbueno@hotmail.com>
2024-06-20 21:56:39 -0300

    criando novos elementos

---
+++ js/criarVideo.js
@@ -0,0 +1,9 @@
+const formulario = document.querySelector("[data-formulario]");
+
+function criarVideo() {
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+
+}
