e8729fe10da71bdf0aec5cd93af1971f8181efd8
Sabrina Bueno <bina_fbueno@hotmail.com>
2024-06-22 16:38:37 -0300

    busca video

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -24,7 +24,14 @@ async function criaVideo(titulo, descricao, url, imagem) {
     return conexaoConvertida;
 }
 
+async function buscaVideos(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideos
 }
