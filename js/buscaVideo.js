3c83953c7b040efdc117c0fb64455324fb883c79
Sabrina Bueno <bina_fbueno@hotmail.com>
2024-06-22 17:09:01 -0300

    reutilizando funções

---
+++ js/buscaVideo.js
@@ -0,0 +1,17 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaApi.buscaVideos(dadosDePesquisa);
+
+    const lista = document.querySelector("[data-lista]");
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento));
